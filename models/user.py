fab19d53ae1da0e1fc2e42ea72be1bbee94fbd37
trOyYeyY <troyzuwara@gmail.com>
2024-03-21 00:33:12 +0000

    user DBStorage

--- models/user.py
+++ models/user.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from os import getenv
 
-
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
+        __tablename__ = 'users'
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+    else:
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
