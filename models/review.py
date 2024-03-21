4df1b640947dc52f753cbeb1af17ed2737a20bed
trOyYeyY <troyzuwara@gmail.com>
2024-03-21 01:36:39 +0000

    minor fix

--- models/review.py
+++ models/review.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey, Integer, Float
 from os import getenv
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review classto store review information """
     if 'db' == getenv("HBNB_TYPE_STORAGE"):
         __tablename__ = "reviews"
