f3dec1b16d91196730145cee1d24bd02788cd63d
trOyYeyY <troyzuwara@gmail.com>
2024-03-21 01:00:08 +0000

    minor fix

--- models/place.py
+++ models/place.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
 from sqlalchemy.orm import relationship
 from os import getenv
