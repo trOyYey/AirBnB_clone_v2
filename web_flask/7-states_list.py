650660e48d2890ad40b9cb6366f03d6cd69ce451
trOyYey <troyzuwara@gmail.com>
2024-04-22 00:22:08 +0000

    task 8 state list

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+    dynamic state list module
+"""
+from flask import Flask, render_template
+from models.state import State
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_storage(exception):
+    """ terminates connection between the app and the database """
+    storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """
+        defining states list route
+        accesses the data from storage backend
+    """
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
