a8aabefb7173adc7d7795cd188b0570ab40b0d9e
trOyYey <troyzuwara@gmail.com>
2024-05-07 04:10:01 +0000

    pycode

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""  Fabric script that creates and distributes an archive to your web servers """
+"""  Fabric script that creates and distributes
+     an archive to your web servers """
 from os.path import getsize
 from fabric.api import *
 do_pack = __import__('1-pack_web_static').do_pack
 do_deploy = __import__('2-do_deploy_web_static').do_deploy
-env.hosts = ['54.209.125.126', '54.85.96.138']
+env.hosts = ['52.86.126.184', '18.207.142.29']
 
 
 pack_path = do_pack()
@@ -14,6 +15,6 @@ def deploy():
     """ deploy the archive """
     if pack_path is None:
         return False
-    print(f"web_static packed: {pack_path} -> {getsize(pack_path)}Bytes")
-    do_deploy(pack_path)
-    return True
+    print(f"web_static packed: {pack_path} -> "
+          f"{getsize(pack_path)}Bytes")
+    return do_deploy(pack_path)
